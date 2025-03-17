5b3960d81a6a54a6122ad8a0e4d2b673cb5da87f
UR <sean.costelloe.7e5@itb.cat>
2025-03-17 07:32:55 -0700

    feat:

--- MyToolbarJog-main/com.ur.urcap.examples.mytoolbarjog/src/main/java/com/ur/urcap/examples/mytoolbarjog/toolbar/TweezerToolbarService.java
+++ MyToolbarJog-main/com.ur.urcap.examples.mytoolbarjog/src/main/java/com/ur/urcap/examples/mytoolbarjog/toolbar/TweezerToolbarService.java
@@ -1,6 +1,12 @@
 package com.itb.tweezer.toolbar;
 
+import java.awt.Image;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
 import javax.swing.Icon;
+import javax.swing.ImageIcon;
 
 import com.ur.urcap.api.contribution.toolbar.ToolbarConfiguration;
 import com.ur.urcap.api.contribution.toolbar.ToolbarContext;
@@ -10,11 +16,6 @@ import com.ur.urcap.api.domain.data.DataModel;
 
 public class TweezerToolbarService implements SwingToolbarService {
 	
-	public TweezerToolbarService() {
-		
-	}
-	
-	//toDo: problems al pasar el data model
 	@Override
 	public SwingToolbarContribution createToolbar(ToolbarContext context) { 
 		return new TweezerToolbarContribution(context);
@@ -22,8 +23,13 @@ public class TweezerToolbarService implements SwingToolbarService {
 	
 	@Override
 	public Icon getIcon() {
-		// TODO Auto-generated method stub
-		return null;
+		BufferedImage image = null;
+		try {
+			image = ImageIO.read(getClass().getResource("/icons/tweezerIcon.png"));
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return new ImageIcon(image.getScaledInstance(30, 30, Image.SCALE_SMOOTH));
 	}
 
 	@Override
--- urcap/com.itb.tweezer/src/main/java/com/itb/tweezer/Activator.java
+++ urcap/com.itb.tweezer/src/main/java/com/itb/tweezer/Activator.java
@@ -1,7 +1,11 @@
-package com.itb.tweezer;
+package com.itb.tweezer.impl;
+
 
 import org.osgi.framework.BundleActivator;
 import org.osgi.framework.BundleContext;
+
+import com.itb.tweezer.toolbar.TweezerToolbarService;
+import com.ur.urcap.api.contribution.toolbar.swing.SwingToolbarService;
 
 /**
  * Hello world activator for the OSGi bundle URCAPS contribution
@@ -11,6 +15,7 @@ public class Activator implements BundleActivator {
 	@Override
 	public void start(BundleContext bundleContext) throws Exception {
 		System.out.println("Activator says Hello World!");
+		bundleContext.registerService(SwingToolbarService.class, new TweezerToolbarService(), null);
 	}
 
 	@Override
@@ -18,4 +23,5 @@ public class Activator implements BundleActivator {
 		System.out.println("Activator says Goodbye World!");
 	}
 }
+
 
