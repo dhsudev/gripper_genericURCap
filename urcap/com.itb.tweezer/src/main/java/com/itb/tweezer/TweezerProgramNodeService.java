cbaa3733805ba0ffbdcc0db704fda4d3150db288
elkin-pinto <elkin.pinto.7e7@itb.cat>
2025-03-20 17:00:38 +0100

    Cambions david program node

---
+++ urcap/com.itb.tweezer/src/main/java/com/itb/tweezer/TweezerProgramNodeService.java
@@ -0,0 +1,49 @@
+package com.itb.tweezer;
+
+import java.io.InputStream;
+import java.util.Locale;
+
+import javax.swing.text.Style;
+
+import com.ur.urcap.api.domain.URCapAPI;
+import com.ur.urcap.api.domain.data.DataModel;
+
+public class TweezerProgramNodeService
+		implements SwingProgramNodeService<TweezerProgramNodeContribution, TweezerProgramNodeView> {
+
+	public TweezerInstalationNodeService() {
+	
+	}
+
+	@Override
+	public String getId() {
+		return "TweezerSwingNode";
+	}
+
+	@Override
+	public void configureContribution(ContributionConfiguration configuration) {
+		configuration.setChildrenAllowed(true);
+	}
+
+	@Override
+	public String getTitle() {
+		String title = "Tweezer Manager";
+		return title;
+	}
+
+	@Override
+	public TweezerProgramNodeView createView(ViewAPIProvider apiProvider) {
+		SystemAPI systemAPI = apiProvider.getSystemAPI();
+		Style style = systemAPI.getSoftwareVersion().getMajorVersion() >= 5 ? new V5Style() : new V3Style();
+		return new TweezerProgramNodeView(style);
+	}
+
+	@Override
+	public TweezerProgramNodeContribution createNode(
+			ProgramAPIProvider apiProvider,
+			TweezerProgramNodeView view,
+			DataModel model,
+			CreationContext context) {
+		return new TweezerProgramNodeContribution(apiProvider, view, model);
+	}
+}
